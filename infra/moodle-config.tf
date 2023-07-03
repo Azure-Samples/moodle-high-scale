866805ebb753596f53e7039c191530b44ee71e76
Guilherme França <guilherme@guilherme.im>
2023-07-03 11:04:24 -0300

    removing unecessary config

--- infra/moodle-config.tf
+++ infra/moodle-config.tf
@@ -86,31 +86,3 @@ resource "kubernetes_secret" "moodle-config" {
   }
 
 }
-
-
-resource "kubernetes_secret" "moodle-config-cron" {
-  metadata {
-    name = "moodle-config-cron"
-    namespace = "moodle"
-  }
-  
-  data = {
-    "STG_NAME"            = azurerm_storage_account.moodle-assets.name
-    "STG_CONTAINER_NAME"  = azurerm_storage_container.moodle-assets.name
-    "STG_SAS_TOKEN"       = trimprefix(data.azurerm_storage_account_blob_container_sas.sastoken.sas, "?")
-    "REDIS_SESSION_HOST"  = "redis-cluster-svc"
-    "REDIS_SESSION_PORT"  = "6379"
-    "DATABASE_HOST"       = "localhost"
-    "DATABASE_PORT"       = "6432"
-    "DATABASE_NAME"       = "citus"
-    "DATABASE_USER"       = "citus"
-    "DATABASE_PASSWORD"   = azurerm_cosmosdb_postgresql_cluster.moodle-cosmos-pgsql.administrator_login_password
-    "DATABASE_PREFIX"     = "md_"
-    "WWW_ROOT"            = "https://${azurerm_cdn_frontdoor_endpoint.moodle-front-door.host_name}"
-    "DATA_ROOT"           = "/var/www/moodledata"
-    "ADMIN"               = "admin"
-    "SSL_PROXY"           = "true"
-    "CFG"                 = "$CFG"
-  }
-
-}
